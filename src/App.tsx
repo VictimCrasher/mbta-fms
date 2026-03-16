bc394f6f96674ea86f0fb1f023986ac269bacb98
Achmad Firdaus Adinegoro <achmadfirdausadinegoro@gmail.com>
2026-03-16 16:25:18 +0700

    feat: Initial push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-primary min-h-screen min-w-screen">
+      <h1 className="text-3xl font-bold p-4">
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
